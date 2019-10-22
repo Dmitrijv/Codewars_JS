70986a26b5470eb364475261f5102aa26935dcbc
Dmitrijv <>
2019-10-22 19:09:50 +0200

    solved file finder kata

--- file_finder/filefinder.js
+++ file_finder/filefinder.js
@@ -2,20 +2,6 @@
 
 through a virtual filesystem of sorts to find a file, then return the path to the file. For example:
 
-let files = {
-  'New folder': {
-    'New folder': {}
-  },
-  'New folder (1)': {
-    'New folder': {
-      'funnyjoke.txt': 'lol i pranked you!!!'
-    }
-  },
-  'New folder (2)': {}
-};
-
-console.log(search(files)); // New folder (1)/New folder/funnyjoke.txt
-
 Sometimes there might not be a file in the filesystem. In that case, just throw an error.
 You're always guaranteed to receive a filesystem with either one or zero files.
 
@@ -23,39 +9,51 @@ Good luck!
 
  */
 
+let files = {
+    'New folder': {
+        'New folder': {}
+    },
+    'New folder (1)': {
+        'New folder': {
+            'funnyjoke.txt': 'lol i pranked you!!!'
+        }
+    },
+    'New folder (2)': {}
+};
 
+console.log(search(files)); // New folder (1)/New folder/funnyjoke.txt
 
 
 
 function search(files) {
 
-    let path = "";
-    let fullPath = getFilePath(path, files);
+    let root = "";
+    const pathToFile = getFirstValidFile(root, files);
 
-    if (!fullPath)
-    //throw new Error('No files!');
-        return fullPath;
+    if (!pathToFile)
+        throw new Error('No files!');
 
-    function getFilePath(path, files){
+    return pathToFile.substr(1);
 
-        //console.log(path);
+    function getFirstValidFile(directory, files) {
 
-        for (let key of Object.keys(files)) {
+        for (let fileName of Object.keys(files)) {
 
-            console.log(key)
-            console.log(files[key])
+            const localDirectory = directory + "/" + fileName;
+            const fileContent = files[fileName];
 
-            if ( (typeof files[key]) !== 'object'){
-                console.log("MATCH!");
-                return path + "/" + key;
-            }
+            if (typeof fileContent === 'string')
+                return localDirectory;
 
-            if ((typeof files[key]) === 'object' && Object.keys(files[key]).length > 0){
-                console.log(" recursive call");
-                return getFilePath(path + "/" + key, files[key]);
+            if ((typeof fileContent === 'object')){
+                const pathToFile = getFirstValidFile(localDirectory, fileContent);
+                if (pathToFile)
+                    return pathToFile;
             }
 
         }
+
+        return null;
 
     }
 
