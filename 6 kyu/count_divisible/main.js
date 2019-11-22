8408570a664f68feb39378ecd88003a2da5d073e
Dmitrij <dmitrij.vel@live.se>
2019-11-22 10:34:49 +0100

    work on count divisible

--- 6 kyu/count_divisible/main.js
+++ 6 kyu/count_divisible/main.js
@@ -10,3 +10,17 @@ Given x = 6, y = 11, k = 2 the function should return 3, because there are three
 divisible by 2 between 6 and 11: 6, 8, 10
 
 */
+
+//101 9007199254740991 11
+
+console.log(divisibleCount(6, 11, 2));
+
+function divisibleCount(x, y, k) {
+  const arr = [];
+  while (x <= y) {
+    arr.push(x);
+    x++;
+  }
+  console.log(arr);
+  return arr.filter(e => e % k === 0).length;
+}
