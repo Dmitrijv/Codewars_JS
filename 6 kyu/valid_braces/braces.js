6e176cb39b442f29d7437155fd2bf6d55e379c85
Dmitrij Velström <dmitrij.vel@live.se>
2020-02-13 19:20:21 +0100

    Update braces.js

--- 6 kyu/valid_braces/braces.js
+++ 6 kyu/valid_braces/braces.js
@@ -4,7 +4,6 @@ Write a function that takes a string of braces, and determines if the order of t
 It should return true if the string is valid, and false if it's invalid.
 
 This Kata is similar to the Valid Parentheses Kata, but introduces new characters: brackets [], and curly braces {}.
-Thanks to @arnedag for the idea!
 
 All input strings will be nonempty, and will only consist of parentheses, brackets and curly braces: ()[]{}.
 What is considered Valid?
